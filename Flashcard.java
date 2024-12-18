e43b7eb8d4582f9beb1d4d579da8373f1bed2081
Surge4315 <surge.4315@gmail.com>
2024-12-18 02:21:46 +0100

    added additional stuff to flashcards so polymorphism will work better

--- Flashcard.java
+++ Flashcard.java
@@ -1,19 +1,24 @@
 abstract class Flashcard {
     public boolean is_correct = false; //checks if answer is correct
     protected String question; //the question.
+    public char type; //type of flashcard (so programme knows what to draw on screen)
 
     abstract void printOut(); //prints out the flashcard for usage
     abstract void printOutDebug(); //prints out the flashcard for debugging
     abstract public String toString();
+    abstract void checkAnswer();
+    abstract void overwriteValues(String new_message, int which_one); //function for changing content of flashcards
 }
 
 class FlashcardText extends Flashcard {
 
-private String answer; //answer to the question
+    private String answer; //answer to the question
+    private String input_answer =""; //user's inputted answer, nothing by default
 
     FlashcardText(String question, String answer) {
       this.question = question;
       this.answer = answer;
+      this.type = 't'; //t -> text
     }
 
     @Override
@@ -28,12 +33,34 @@ private String answer; //answer to the question
     }
 
     @Override
+    void overwriteValues(String new_message, int which_one) {
+        switch(which_one) { //choosing what to overwrite
+            case(0): {
+                question = new_message;
+                break;
+            }
+            case(1): {
+                answer = new_message;
+                break;
+            }
+            case(2): {
+                input_answer = new_message; //btw this whole overwriting values function exists as an excuse for being able to save the input answer somewhere
+                break;
+            }
+        }
+
+    }
+
+    @Override
     public String toString(){
         return "question: " + question + " answer: " + answer;
     }
 
-    void checkAnswer(String input) {
-        if(input == answer){
+
+
+    @Override
+    void checkAnswer() {
+        if(input_answer == answer){
             is_correct = true;
         }
         //no need to change to 0 it already is 0
